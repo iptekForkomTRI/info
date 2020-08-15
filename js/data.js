23d75dd01f032b18a39bbae8e4cc7b1436f74286
iptekForkomTRI <69426785+iptekForkomTRI@users.noreply.github.com>
2020-08-15 09:13:54 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -12,6 +12,16 @@ const dataSeminar = [
         link : "https://dilo.id/Event/Detail/1500",
         gambar : "https://minio.dilo.id/event/39f3723c-a714-48d8-b4e0-a7f6a3d8cba4.png",
         judul : "IT DEVELOPMENT ON HEALTH STARTUP"
+    },
+    {
+        link : "https://www.instagram.com/p/CD3gf1dHhYR/",
+        gambar : "https://instagram.fpku3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/117600835_191678748988226_7128874179978585288_n.jpg?_nc_ht=instagram.fpku3-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=G4Irqj3btUoAX_xTn_a&oh=d04943fa9eb1a4e784bd75f2557db9fb&oe=5F628944",
+        judul : "Discovering Cloud Solutions for Business Continuity"
+    },
+    {
+        link : "https://www.instagram.com/p/CD3LzmNnYIK/",
+        gambar : "https://instagram.fpku2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/117376927_1224631081230021_3785803182273914555_n.jpg?_nc_ht=instagram.fpku2-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=41pbj5wCMzAAX94cwNm&oh=64abf37e10b4a3f0db9e269a4253d070&oe=5F604311",
+        judul : "Learn Google Analythics"
     }
 
 ];
@@ -40,5 +50,15 @@ const dataPelatihan = [
         link : "https://readysetcode.id",
         gambar : "https://pbs.twimg.com/media/EfH_lopUMAARPsG?format=png&name=small",
         judul : "Ready, Set, Code!"
+    },
+    {
+        link : "https://www.instagram.com/p/CD31LVlHAwW/",
+        gambar : "https://instagram.fpku3-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/117385474_173570280877945_5700147703666072386_n.jpg?_nc_ht=instagram.fpku3-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=Ed4kjrID0DgAX9tTKZp&oh=3498581053d028b0f52f3922798988fe&oe=5F626B9B",
+        judul : "Reactjs : Concept & Implementation"
+    },
+    {
+        link : "https://www.instagram.com/p/CD3uXlzjYTm/",
+        gambar : "https://instagram.fpku2-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/117748278_1231020147233296_610732120123324224_n.jpg?_nc_ht=instagram.fpku2-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=NyzZTjhaLHMAX9qEEeF&oh=53f8b281d355312bf594c262ce7459d6&oe=5F62A531",
+        judul : "UX Research"
     }
 ];
