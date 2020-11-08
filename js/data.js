2ad5b6bed498d5090fd6d99e05dbbd94fa6fc374
iptekForkomTRI <69426785+iptekForkomTRI@users.noreply.github.com>
2020-11-08 12:08:15 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -8,6 +8,26 @@
 
 
 const dataSeminar = [
+    {
+        link : "https://dilo.id/Event/Detail/2189",
+        gambar : "https://minio.dilo.id/event/108958c3-4dc5-4f54-a766-c388c6cbc344.jpeg",
+        judul : "Pengenalan Data Science Bagi Pemula"
+    },
+    {
+        link : "https://dilo.id/Event/Detail/2191",
+        gambar : "https://minio.dilo.id/event/1db98100-54ed-4a8a-b9db-bdd56fa8afae.jpeg",
+        judul : "Pengenalan Programming Flutter Untuk Pengembangan Aplikasi Mobile"
+    },
+    {
+        link : "https://dilo.id/Event/Detail/2208",
+        gambar : "https://minio.dilo.id/event/7e8f8523-8f09-4edf-82fc-4aaf4a516890.png",
+        judul : "DILo A Conference “Cyber Security Strategy”"
+    },
+    {
+        link : "https://dilo.id/Event/Detail/2160",
+        gambar : "https://minio.dilo.id/event/51fcea94-cb26-4ea7-9f57-c2f2b4dd86e6.png",
+        judul : "How To Implement A Scrum Methodology in Your Startup"
+    },
     
 ];
 
