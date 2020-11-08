08c85b9aaf1b9b99eb2ccc751167be9de0e02c28
iptekForkomTRI <69426785+iptekForkomTRI@users.noreply.github.com>
2020-11-08 11:30:25 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -8,85 +8,22 @@
 
 
 const dataSeminar = [
-    {
-        link : "https://dilo.id/Event/Detail/1897",
-        gambar : "https://minio.dilo.id/event/354a0e68-b79c-4663-8672-a7378d4d2dad.png",
-        judul : "Accounting for Startup"
-    },
-    {
-        link : "https://dilo.id/Event/Detail/1923",
-        gambar : "https://minio.dilo.id/event/90b38c2d-e825-49fe-b197-f30b84ed717e.png",
-        judul : "The Power of SCRUM Method for Startup Development"
-    },
-    {
-        link : "https://dilo.id/Event/Detail/1926",
-        gambar : "https://minio.dilo.id/event/1beb5d26-6fe1-4471-be2e-318547c33a7c.png",
-        judul : "Public Speaking for Business"
-    },
-    {
-        link : "https://dilo.id/Event/Detail/1898",
-        gambar : "https://minio.dilo.id/event/a4af65eb-6340-4f5b-9570-e5ea6a67f580.png",
-        judul : "Know Your Life Know Your Strength"
-    },
-    {
-        link : "https://dilo.id/Event/Detail/1931",
-        gambar : "https://minio.dilo.id/event/45de390c-c90b-4694-9142-78bc1536bc1e.png",
-        judul : "Digital Marketing Tips and Trick for Business"
-    },
-    {
-        link : "https://dilo.id/Event/Detail/1930",
-        gambar : "https://minio.dilo.id/event/23a0b90b-9190-4cb5-9390-598f02219f97.png",
-        judul : "IT Supply Chain For Startup 4.0"
-    },
-    {
-        link : "https://dilo.id/Event/Detail/1924",
-        gambar : "https://minio.dilo.id/event/11669cc3-cf33-45e9-ac5f-51aa7d550fd5.jpg",
-        judul : "TRANSFORMASI DIGITAL DAN PENGARUHNYA PADA BISNIS & SDM"
-    },
-    
     
 ];
 
 const dataLomba = [
     {
-        link : "https://www.instagram.com/p/CF1Ob5jJ7Mn/",
-        gambar : "https://instagram.fkno6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.134.1080.1080a/s640x640/120358139_767724600683254_1673076820030021748_n.jpg?_nc_ht=instagram.fkno6-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=xbK0c4kBO0YAX9bn-HA&oh=d750cd5180c77dd9421dc23d093eca0b&oe=5FA24C2B",
-        judul : "Informatics Competition Himpunan Mahasiswa Teknik Informatika Universitas Ahmad Dahlan"  
-    },
-    {
         link : "https://careers.shopee.co.id/ndsc/",
         gambar : "https://careers.shopee.co.id/ndsc/images/hero-image-2020.png",
         judul : "National Data Science Challenge 2020"
     },
-    {
-        link : "https://www.openstack.id/2020/10/08/open-infrastructure-cloud-native-day-indonesia-2020/?fbclid=IwAR317fkTQFORjdJHHWccJpSQQB7kBOhXflk2-4yI6769u1sXaZOcwIm_puc",
-        gambar : "https://www.openstack.id/wp-content/uploads/2020/10/register-oicndi20-10102020.jpg",
-        judul : "Open Infrastructure & Cloud Native Day Indonesia 2020"
-    },
-    {
-        link : "https://youtube.com/MikrotikIndonesiaCitraweb",
-        gambar : "js/photo_2020-10-12_21-24-21.jpg",
-        judul : "MUM INDONESIA 2020"
-    },
-   
     
 ];
 
 const dataPelatihan = [
     {
-        link : "https://dilo.id/Event/Detail/1897",
-        gambar : "https://minio.dilo.id/event/a64babc0-a6d3-4011-b77d-c03cdf4f10d8.jpg",
-        judul : "BELAJAR DASAR DESAIN LOGO"
-    },
-    {
         link : "https://kelaskita.com/lab127/kelas/lab127-networking-essentials/",
         gambar : "https://kelaskita.com/media/images/2019/09/2019-09-09-18-56-24823075_logolab127-(1)png594.jpg",
         judul : "LAB127 - Networking Essentials"
-    },
-    {
-        link : "https://docs.google.com/forms/d/e/1FAIpQLSc5-YQx51cVnNyNHHu5rzEyGdnxHxSa2QbT9uLXgZWk-IB2qg/viewform",
-        gambar : "https://scontent.fjog4-1.fna.fbcdn.net/v/t1.0-0/p526x296/120040149_179615006948062_3714860996794592193_o.jpg?_nc_cat=111&_nc_sid=730e14&_nc_ohc=H-5x6QJ1mqkAX82NDMy&_nc_oc=AQn4dyBlm2LppF1i-1pBnKWvkC-NUpQN4S19nfgLIWv6BWl9szRO5cnstEWoh5jyLG8&_nc_ht=scontent.fjog4-1.fna&tp=6&oh=bbb81ec4c584a5a609be90df3bbfaeee&oe=5FA9D76B",
-        judul : "Data Science Training"
-    },
-    
+    },  
 ];
