e441a358804302c4511254fe155392512088fe15
iptekForkomTRI <69426785+iptekForkomTRI@users.noreply.github.com>
2021-03-17 13:31:30 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -21,6 +21,11 @@ const dataLomba = [
 
 const dataPelatihan = [
     {
+        link : "http://aws.dicoding.com/",
+        gambar : "https://media-exp1.licdn.com/dms/image/C5622AQG3vfgmlELG_Q/feedshare-shrink_800/0/1615795310562?e=1619049600&v=beta&t=AUzrA9PuxJO1CtxldwBUENZVUOAxSGqPbD35y4Vmggo",
+        judul : "Cloud and Back-End Developer Scholarship Program"
+    },  
+    {
         link : "https://kelaskita.com/lab127/kelas/lab127-networking-essentials/",
         gambar : "https://kelaskita.com/media/images/2019/09/2019-09-09-18-56-24823075_logolab127-(1)png594.jpg",
         judul : "LAB127 - Networking Essentials"
